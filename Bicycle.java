21a28de5c8b800b978eb3ad3326984f33d86a06e
Fabricio Sousa <fabriciosousa@Mobiworks-iMac-2.local>
2018-07-13 11:59:06 -0400

    Make Bicycle class

---
+++ Bicycle.java
@@ -0,0 +1,15 @@
+class Bicycle {
+    private String name;
+
+    Bicycle(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void Main(String[] args) {
+        Bicycle Bicycle = new Bicycle("Bianchi");
+        System.out.println("[Bicycle Info]");
+        System.out.println("Name: " + Bicycle.getName());
+        
+
+    }
+}
